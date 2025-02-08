5156584302b6d7c0b8a14ee1b78adfee6317f1c9
Eduardo Marchi <eduardo.marchi@gmail.com>
2025-02-08 17:57:32 -0300

    Update app.js
    reescrevendo os códigos que haviam  sofrido alterações erradas.

--- app.js
+++ app.js
@@ -1,159 +1,140 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = [];
-let sorteioRealizado = false;
-let toRemove = '';
-let sorteado = '';
-
-// Coloca a primeira letra de cada palavra em maiúscula
-function capitalizar(texto) {
-    return texto.split(' ')
-        .map(palavra => palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase())
-        .join(' ');
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    color: white;
 }
 
-// Verifica se o campo está vazio
-function validaCampoVazio(texto) {
-    return texto === '';
+body {
+    background: linear-gradient(#1354A5 0%, #041832 33.33%, #041832 66.67%, #01080E 100%);
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
-// Adicionar evento de keydown para adicionar amigo ao pressionar Enter
-document.getElementById("amigo").addEventListener("keydown", function (event) 
-{
-  
-  if (event.key === "Enter") {
-    adicionarAmigo();
-  }
-});
 
-listaDeAmigos.push(nomeAmigo);
-  atualizarListaDeAmigos();
-  inputAmigo.value = "";
-  
-// Verifica se o amigo já foi adicionado
-function validaDuplicado(amigo) {
-    return amigos.includes(amigo);
+body::before {
+    background-image: url("img/code.png");
+    background-repeat: no-repeat;
+    background-position: right;
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 0.4;
 }
 
-function validaTrataInput(amigoInput) {
+.container {
+    width: 80%;
+    height: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 24px;
+    border: 1px solid #1875E8;
+    box-shadow: 4px 4px 20px 0px rgba(1, 8, 14, 0.15);
+    background-image: url("img/Ruido.png");
+    background-size: 100% 100%;
+    position: relative;
+}
 
-    // remove espaços do começo e final do texto
-    const amigo = capitalizar(amigoInput.trim());
 
-    // verifica se o campo está vazio
-    if (validaCampoVazio(amigo)) {
-        alert('Digite o nome do Amigo!');
-        return;
+.container__conteudo {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    bottom: 0;
+}
+
+.container__informacoes {
+    flex: 1;
+    padding: 3rem;
+}
+
+.container__botao {
+    border-radius: 16px;
+    background: #1875E8;
+    padding: 16px 24px;
+    width: 100%;
+    font-size: 24px;
+    font-weight: 700;
+    border: none;
+    margin-top: 2rem;
+}
+
+.container__texto {
+    margin: 16px 0 16px 0;
+}
+
+.container__texto-azul {
+    color: #1875E8;
+}
+
+.container__input {
+    width: 100%;
+    height: 72px;
+    border-radius: 16px;
+    background-color: #FFF;
+    border: none;
+    color: #1875E8;
+    padding: 2rem;
+    font-size: 24px;
+    font-weight: 700;
+    font-family: 'Inter', sans-serif;
+}
+
+.container__botoes {
+    display: flex;
+    gap: 2em;
+}
+
+h1 {
+    font-family: 'Chakra Petch', sans-serif;
+    font-size: 72px;
+    padding-bottom: 3rem;
+}
+
+p,
+button {
+    font-family: 'Inter', sans-serif;
+}
+
+.texto__paragrafo {
+    font-size: 32px;
+    font-weight: 400;
+}
+
+button:disabled {
+    background-color: gray;
+}
+
+@media screen and (max-width: 1250px) {
+
+    h1 {
+        font-size: 50px;
     }
 
-    // valida se o nome possui apenas letras
-    if (!/^[a-zA-Z\s]+$/.test(amigo)) {
-        alert(`O nome ${amigo} é inválido! O nome deve conter apenas letras!`);
-        return;
+    .container__botao {
+        font-size: 16px;
     }
 
-    // verifica se o amigo já foi adicionado
-    if (validaDuplicado(amigo)) {
-        alert(`O nome do Amigo ${amigo} já foi adicionado, coloque outro!`);
-        return;
-    }
-    amigos.push(amigo);
-}
 
-
-// Adiciona um amigo à lista
-function adicionarAmigo() {
-    const amigoInput = document.getElementById('amigo').value;
-
-    //adiciona amigos separados por vírgula
-    if (amigoInput.includes(',')) {
-        //split a string em um array
-        let amigosInput = amigoInput.split(',');
-        for (let item of amigosInput) {
-           validaTrataInput(item);
-        }
-    }else{
-        validaTrataInput(amigoInput);
+    .texto__paragrafo {
+        font-size: 24px;
     }
 
-    document.getElementById('resultado').innerHTML = '';
-    atualizarListaAmigos();
-    limparElemento("amigo");
-}
-
-// Sorteia um amigo da lista
-function sortearAmigo() {
-    if (sorteioRealizado) {
-        alert('Todos os nomes já foram sorteados, adicione novos amigos para sortear novamente!');
-        ocultarResultado();
-        return;
-    } 
-    if (amigos.length === 0) {
-        alert('Adicione amigos para sortear!');
-        return;
-    } 
-    
-    if(toRemove){
-        amigos = amigos.filter(amigo => amigo !== toRemove);
-        toRemove = '';
+    .container__imagem-pessoa {
+        display: none;
     }
-    
-    sorteado = amigos[Math.floor(Math.random() * amigos.length)];
-    limparElemento('amigo');
-    limparElemento('listaAmigos');
-    if (amigos.length == 1) {
-        alterarValorElemento('resultado', sorteado);
-        document.getElementById("hiden-elements").classList.remove("hidden");
-        document.getElementById("auto-sorteio").classList.add("hidden");
-        sorteioRealizado = true;
-    }else{
-        alterarValorElemento('resultado', sorteado);
-        document.getElementById("hiden-elements").classList.remove("hidden");
-        document.getElementById("auto-sorteio").classList.remove("hidden");
-        console.log("removeu");
-        toRemove = sorteado;
+
+    .container__conteudo {
+        display: block;
+        position: inherit;
     }
-    let mensagem = "O Amigo Secreto Sorteado é: " + sorteado;
-    alterarValorElemento('resultado', mensagem);
-}
 
-// Atualiza a lista de amigos no HTML
-function atualizarListaAmigos() {
-    alterarValorElemento('listaAmigos', amigos.map(amigo => `<li>${amigo}</li>`).join(''));
-}
-
-// Limpa a lista de amigos no HTML
-function reiniciarAmigoSecreto() {
-    limparElemento('amigo');
-    limparElemento('resultado');
-    limparElemento('listaAmigos');
-    document.getElementById("hiden-elements").classList.add("hidden");
-    amigos = [];
-    sorteioRealizado = false;
-    sorteado = '';
-    toRemove = '';
-}
-
-
-function ocultarResultado(){
-    if (!sorteioRealizado) {
-        alterarValorElemento('resultado', 'Continue sorteando!');
-        document.getElementById("auto-sorteio").classList.remove("hidden");
-        document.getElementById("hiden-elements").classList.add("hidden");
+    .container__informacoes {
+        padding: 1rem
     }
-    else{
-        alterarValorElemento('resultado', '');
-        reiniciarAmigoSecreto();
-    }
-    document.getElementById("auto-sorteio").classList.remove("hidden");
-    document.getElementById("hiden-elements").classList.add("hidden");
-}
-
-// Limpa o valor de um elemento
-function limparElemento(elemento) {
-    alterarValorElemento(elemento, '');
-    document.getElementById(elemento).value = '';
-}
-
-function alterarValorElemento(elemento, valor) {
-    document.getElementById(elemento).innerHTML = valor;
 }
